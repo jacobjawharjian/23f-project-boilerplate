4834813caf5398886b23ef5a54b66f99d672f29a
rishi2940 <125758645+rishi2940@users.noreply.github.com>
2024-04-18 21:27:15 -0400

    Update __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -36,12 +36,14 @@ def create_app():
     from src.customers.customers import customers
     from src.products.products  import products
     from src.students.students  import students
+    from src.meals.meals  import meals
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(students,    url_prefix='/s')
+    app.register_blueprint(meals,    url_prefix='/m')
 
     # Don't forget to return the app object
     return app
